5e275ab448881366146cc861ea1363dcfa307b5d
bala-amypo <bala@amypo.in>
2025-12-16 06:35:42 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
-@Configuration
+demo/src/main/java/com/example/demo/config@Configuration
 public class OpenApiConfig {
 
     @Bean
