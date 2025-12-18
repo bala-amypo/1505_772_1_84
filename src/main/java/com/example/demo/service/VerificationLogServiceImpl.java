abf6a3ef639e803b446783e184dd285f6a99f058
bala-amypo <bala@amypo.in>
2025-12-18 15:52:05 +0000

    User commit

--- src/main/java/com/example/demo/service/VerificationLogServiceImpl.java
+++ src/main/java/com/example/demo/service/VerificationLogServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.demo.service;
 
-
 import com.example.demo.model.VerificationLogEntity;
 import com.example.demo.repository.VerificationLogRepository;
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class VerificationLogServiceImpl implements VerificationLogService {
 
@@ -13,16 +14,19 @@ public class VerificationLogServiceImpl implements VerificationLogService {
         this.repo = repo;
     }
 
+    @Override
     public VerificationLogEntity createLog(VerificationLogEntity log) {
         return repo.save(log);
     }
 
+    @Override
     public List<VerificationLogEntity> getLogsForEntry(Long entryId) {
         return repo.findByServiceEntryId(entryId);
     }
 
+    @Override
     public VerificationLogEntity getLogById(Long id) {
         return repo.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Log not found"));
+                .orElseThrow(() -> new NoSuchElementException("Log not found"));
     }
 }
