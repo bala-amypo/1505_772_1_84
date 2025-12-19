03422bc4fd22dbc705a77c5d2880b12b1448fb97
bala-amypo <bala@amypo.in>
2025-12-19 08:41:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/ServicePartRepository.java
+++ src/main/java/com/example/demo/repository/ServicePartRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.ServicePartEntity;
+import com.example.demo.model.ServicePart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
-public interface ServicePartRepository extends JpaRepository<ServicePartEntity, Long> {
-    List<ServicePartEntity> findByServiceEntryId(Long entryId);
+public interface ServicePartRepository extends JpaRepository<ServicePart, Long> {
+    // No extra methods required as per specification
 }
