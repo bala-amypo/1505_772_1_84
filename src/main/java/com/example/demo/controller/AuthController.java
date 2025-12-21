1605f24fd47b81acd244edab5dc8e7c94a76bb0d
bala-amypo <bala@amypo.in>
2025-12-21 16:00:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,8 +4,6 @@ import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -16,30 +14,13 @@ public class AuthController {
         this.userService = userService;
     }
 
-
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userService.createUser(user);
     }
 
- 
-    @GetMapping("/users")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable Long id) {
-        return userService.getUserById(id);
-    }
-
-    @PutMapping("/users/{id}")
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
-        return userService.updateUser(id, user);
-    }
-
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
+    @GetMapping("/login")
+    public User login(@RequestParam String email) {
+        return userService.getUserByEmail(email);
     }
 }
